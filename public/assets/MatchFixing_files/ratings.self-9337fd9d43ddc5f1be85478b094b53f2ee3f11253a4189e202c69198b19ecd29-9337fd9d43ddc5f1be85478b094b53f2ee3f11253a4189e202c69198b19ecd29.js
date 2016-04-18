ef9b625367d242165075e43d737cd588e063bd60
iferunsewe <iferunsewe@gmail.com>
2016-04-18 17:22:37 +0100

    Added error message for match report if team has no players

---
+++ public/assets/MatchFixing_files/ratings.self-9337fd9d43ddc5f1be85478b094b53f2ee3f11253a4189e202c69198b19ecd29-9337fd9d43ddc5f1be85478b094b53f2ee3f11253a4189e202c69198b19ecd29.js
@@ -0,0 +1,62 @@
+ratingsSection = {};
+
+
+ratingsSection.setStars = function(form_id, stars){
+    for(i=1; i<=5; i++){
+        if(i<=stars){
+            $('#' + form_id + '_' + i).attr("src", "/assets/star-on.png");
+        } else {
+            $('#' + form_id + '_' + i).attr("src", "/assets/star-off.png");
+        }
+    }
+};
+
+specialties = {};
+
+specialties.limitChosenSpecialities = function(){
+    $('.specialities_checkboxes').click(function(){
+        var specialty = $(this);
+        specialty.attr("checked", true);
+        if ($('.specialities_checkboxes:checked').length > 3){
+            specialty.attr("checked", false);
+            console.log("HERE")
+            $(".specialties_checkboxes").not(':checked').attr('disabled', true);
+            alert("You can't have it all. Only 3 specialties can be chosen!");
+        } else {
+            $(".specialties_checkboxes").removeAttr("disabled");
+        }
+    });
+};
+
+
+$(document).ready(function(){
+    $('.btn-rate-player').click()
+    $('.star_rating').click(function(){
+        console.log('RATING');
+        var star = $(this);
+        var form_id = star.attr('data-form-id');
+        var stars = star.attr('data-stars');
+        var object_name = $(".object-name").text();
+        ratingsSection.setStars(form_id, stars);
+        $('#' + form_id + '_stars').val(stars);
+        $.ajax({
+            method: 'POST',
+            url: $('#' + form_id).attr('action'),
+            data: $('#' + form_id).serialize()
+        }).success(function(){
+            $('#ratings_form').html('');
+            $('#ratings_form').append("<p> You've rated " + object_name + "</p>");
+        });
+    });
+
+    $('.star_rating_form').each(function(){
+        var form_id = $(this).attr('id');
+        var stars = $('#' + form_id + '_stars').val();
+        ratingsSection.setStars(form_id, stars)
+    });
+
+    specialties.limitChosenSpecialities();
+});
+
+
+// TO BE REFACTORED!!!
